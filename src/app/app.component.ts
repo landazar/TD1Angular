7a05d600c4ed7a3dab20df9c5e7d29ae8979f556
landazar <lucaspagies@gmail.com>
2023-07-03 09:05:26 +0200

    commit de TD2 angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TD1Ex2';
+  imageUrl:string ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTryn_uiiwXY--Ty51yHavKRNNwBsXEPF29ZQ&usqp=CAU";
+  nomClass = "rouge";
+  isActif=false;
+
+  test() {
+    console.log("Bouton cliqué");
+  }
+
+  utilisateurs = [
+    {"nom" : "Dupont", "prenom" : "Nicolas", "age" : 25, "email" : "test@test;fr", "tel" : "123"},
+    {"nom" : "Durand", "prenom" : "Paul", "age" : 45, "email" : "test1@test;fr", "tel" : "1234"},
+    {"nom" : "Dubois", "prenom" : "François", "age" : 32, "email" : "test2@test;fr", "tel" : "1235"}
+    ]
+
+  isDetails = false;
+  montreDetails()
+  {
+    this.isDetails=true;
+  }
+
+  cacheDetails()
+  {
+    this.isDetails=false;
+  }
+
+  para:string ="";
+
+  user:{nom:string, prenom:string, age:number, email:string, tel:string}={"nom" : "Dupont", "prenom" : "Nicolas", "age" : 25, "email" : "test@test;fr", "tel" : "123"}
+
+  ajouter()
+  {
+    this.user.nom=this.para;
+    this.utilisateurs.push(this.user);
+  }
 }
